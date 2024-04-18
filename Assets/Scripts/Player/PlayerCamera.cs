1ad555da9215b57fcfc722fe3a93148966d41a9c
Emiliano-M-Salvado-F <emiliano.m.salvado.f@gmail.com>
2024-04-18 20:25:53 -0300

    Camera distance when player is behind a wall
    When player is behind a wall and out of sight, the camera is set in a distance relative to the obstacle. A bug remains to be fix.

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
     [SerializeField] Transform _target;
     [SerializeField] Transform _pivot;
-    [SerializeField] float _offset, _sensivity;
+    [SerializeField] float _offset, _blockedOffset, _sensivity;
     float mouseX, mouseY;
+    [SerializeField] LayerMask _everythingButPlayerLayer;
+    RaycastHit _hit;
 
     public Vector3 CameraRelativeForward
     {
@@ -23,7 +26,7 @@ public class PlayerCamera : MonoBehaviour
 
     public void UpdatePivot()
     {
-        _pivot.position = _target.transform.position;
+        _pivot.position = _target.transform.position + Vector3.up * 1f;
         _pivot.forward = transform.forward + (_pivot.transform.position - transform.position);
     }
 
@@ -32,9 +35,32 @@ public class PlayerCamera : MonoBehaviour
         if (x != 0f || y != 0f)
         {
             transform.position += (transform.right * -x + transform.up * -y) * (_sensivity * Time.deltaTime);
-            transform.LookAt(_target);
+            transform.LookAt(_pivot);
         }
 
-        transform.position = _target.position - transform.forward * _offset;
+        transform.position = _pivot.position - transform.forward * CameraDistance();
+    }
+
+    float CameraDistance()
+    {
+        if (Physics.Raycast(transform.position, _pivot.position - transform.position, out _hit, _offset, _everythingButPlayerLayer))
+        {
+            return (_pivot.position - _hit.point).magnitude - _blockedOffset;
+        }
+
+        if (Physics.Raycast(transform.position, transform.position - transform.forward, out _hit, _blockedOffset + .1f, _everythingButPlayerLayer))
+        {
+            return (_pivot.position - _hit.point).magnitude - _blockedOffset;
+        }
+
+        return _offset;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position, _pivot.position - transform.position);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, (transform.position - transform.forward * (_blockedOffset + .2f)));
     }
 }

e899ec438ab85bf77b3779506a179edc2a0a5c61
Emiliano-M-Salvado-F <emiliano.m.salvado.f@gmail.com>
2024-04-18 21:40:04 -0300

    Camera fix and adjustments
    Camera bug fixed. Camera sensivity is relative to how close it gets to its target (avoids its position "going crazy" if it gets too close and is more comfortable to use).

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
     [SerializeField] Transform _target;
     [SerializeField] Transform _pivot;
-    [SerializeField] float _offset, _blockedOffset, _sensivity;
+    [SerializeField] float _offset, _blockedOffset, _defaultSensivity, _auxSensivity;
     float mouseX, mouseY;
     [SerializeField] LayerMask _everythingButPlayerLayer;
     RaycastHit _hit;
@@ -34,7 +35,7 @@ public class PlayerCamera : MonoBehaviour
     {
         if (x != 0f || y != 0f)
         {
-            transform.position += (transform.right * -x + transform.up * -y) * (_sensivity * Time.deltaTime);
+            transform.position += (transform.right * -x + transform.up * -y) * (_auxSensivity * Time.deltaTime);
             transform.LookAt(_pivot);
         }
 
@@ -43,15 +44,13 @@ public class PlayerCamera : MonoBehaviour
 
     float CameraDistance()
     {
-        if (Physics.Raycast(transform.position, _pivot.position - transform.position, out _hit, _offset, _everythingButPlayerLayer))
+        if (Physics.Raycast(_pivot.position - transform.forward * _offset, _pivot.position - transform.position, out _hit, _offset, _everythingButPlayerLayer))
         {
+            _auxSensivity = Mathf.Lerp(_defaultSensivity * .2f, _defaultSensivity, (_pivot.position - transform.position).sqrMagnitude / (_offset * _offset));
             return (_pivot.position - _hit.point).magnitude - _blockedOffset;
         }
 
-        if (Physics.Raycast(transform.position, transform.position - transform.forward, out _hit, _blockedOffset + .1f, _everythingButPlayerLayer))
-        {
-            return (_pivot.position - _hit.point).magnitude - _blockedOffset;
-        }
+        _auxSensivity = _defaultSensivity;
 
         return _offset;
     }
@@ -59,8 +58,8 @@ public class PlayerCamera : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawRay(transform.position, _pivot.position - transform.position);
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, (transform.position - transform.forward * (_blockedOffset + .2f)));
+        Gizmos.DrawRay(_pivot.position - transform.forward * _offset, _pivot.position - transform.position);
+        //Gizmos.color = Color.red;
+        //Gizmos.DrawLine(transform.position, (transform.position - transform.forward * (_blockedOffset + .2f)));
     }
 }

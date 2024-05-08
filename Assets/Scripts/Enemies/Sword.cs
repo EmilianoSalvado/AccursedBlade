e4fa6f855026d91afbaf15412759244e09a1598d
Emiliano-M-Salvado-F <emiliano.m.salvado.f@gmail.com>
2024-05-08 18:15:47 -0300

    Enemy weapons collisions fix

--- Assets/Scripts/Enemies/Sword.cs
+++ Assets/Scripts/Enemies/Sword.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
+using System.Collections;
 
 public class Sword : EnemyWeapon
 {
     [SerializeField] Animator _animator;
     [SerializeField] string _animParameter;
+    [SerializeField] Collider _collider;
+    [SerializeField] float _colliderTime;
     public override void Attack()
     {
         _animator.SetTrigger(_animParameter);
+        StartCoroutine(ColliderEnabling());
+    }
+
+    IEnumerator ColliderEnabling()
+    {
+        _collider.enabled = true;
+        yield return new WaitForSeconds(_colliderTime);
+        _collider.enabled = false;
     }
 }
